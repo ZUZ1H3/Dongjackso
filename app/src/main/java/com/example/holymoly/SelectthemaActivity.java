fb7fd13e569acfce78271413ae64b9d616ca088d
zuzihe <dya_yaaa@naver.com>
2024-07-13 22:29:35 +0900

    activity_selectthema.xml radiobutton으로 수정

--- app/src/main/java/com/example/holymoly/SelectthemaActivity.java
+++ app/src/main/java/com/example/holymoly/SelectthemaActivity.java
@@ -1,4 +1,16 @@
 package com.example.holymoly;
 
-public class SelectthemaActivity {
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelectthemaActivity extends AppCompatActivity {
+
+    private ImageButton btntrophy, btnsetting, btnmaking, btnalbum, btnworld;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selectthema);
+    }
 }
