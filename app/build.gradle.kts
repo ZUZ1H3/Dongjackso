9226e97c795f8e315400848a78a68e06f2ba121a
zuzihe <dya_yaaa@naver.com>
2024-07-16 18:50:42 +0900

    Gemini API 연동, 버전 23으로 내림

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.holymoly"
         minSdk = 23
-        targetSdk = 23
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
