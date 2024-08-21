e51a343da01872f38c8ab2c4e76d1f51c89e42b4
felizsong <jalonemo@naver.com>
2024-08-21 14:12:35 +0900

    [앨범] 어댑터 추가

--- app/src/main/java/com/example/holymoly/BookAdapter.java
+++ app/src/main/java/com/example/holymoly/BookAdapter.java
@@ -6,7 +6,9 @@ import android.graphics.Rect;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -14,27 +16,28 @@ import com.bumptech.glide.Glide;
 
 import java.util.List;
 
+
 public class BookAdapter extends RecyclerView.Adapter<BookViewHolder> {
-    private List<String> imageUrls;
+    private List<String> imageUrls, titles;
     private Context context;
     private OnItemClickListener onItemClickListener;
 
-    public BookAdapter(Context context, List<String> imageUrls) {
+    public BookAdapter(Context context, List<String> imageUrls, List<String> titles) {
         this.context = context;
         this.imageUrls = imageUrls;
+        this.titles = titles;
     }
-
     @Override
     public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.item_books, parent, false);
         return new BookViewHolder(view);
     }
-
     @Override
     public void onBindViewHolder(BookViewHolder holder, int position) {
         String imageUrl = imageUrls.get(position);
+        String title = titles.get(position);
         Glide.with(context).load(imageUrl).into(holder.coverIV);
-
+        holder.title.setText(title);
 
         // 이미지 클릭 리스너 설정
         holder.coverIV.setOnClickListener(v -> {
@@ -43,11 +46,8 @@ public class BookAdapter extends RecyclerView.Adapter<BookViewHolder> {
             }
         });
     }
-
     @Override
-    public int getItemCount() {
-        return imageUrls.size();
-    }
+    public int getItemCount () { return imageUrls.size(); }
 
     // 클릭 리스너 인터페이스 정의
     public interface OnItemClickListener {
@@ -55,18 +55,17 @@ public class BookAdapter extends RecyclerView.Adapter<BookViewHolder> {
     }
 
     // 클릭 리스너 설정 메서드
-    public void setOnItemClickListener(OnItemClickListener listener) {
+    public void setOnItemClickListener (OnItemClickListener listener) {
         this.onItemClickListener = listener;
     }
 }
-
 // recyclerView 어댑터
 class BookViewHolder extends RecyclerView.ViewHolder {
     public ImageView coverIV;
-
-    public BookViewHolder(View imageView) {
-        super(imageView);
-        coverIV = imageView.findViewById(R.id.coversIV);
+    public TextView title;
+    public BookViewHolder(View itemView) {
+        super(itemView);
+        coverIV = itemView.findViewById(R.id.coversIV);
+        title = itemView.findViewById(R.id.tv_title);
     }
 }
-
