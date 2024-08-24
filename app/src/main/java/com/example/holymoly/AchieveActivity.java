c4e69fff787c1ebfcd35aef74a078e8493c86f8d
Joohyun Lee <2271183@hansung.ac.kr>
2024-08-25 03:00:59 +0900

    업적 자바 완성

--- app/src/main/java/com/example/holymoly/AchieveActivity.java
+++ app/src/main/java/com/example/holymoly/AchieveActivity.java
@@ -1,12 +1,16 @@
 package com.example.holymoly;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +19,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AchieveActivity extends AppCompatActivity implements UserInfoLoader {
-    //라디오버튼2개 라디오그룹1 나가기버튼1 등록
-    //동화 업적 - ~~개 만들기 텍스트뷰 수정, 오른쪽에 진행도 퍼센트+보상받기 텍스트뷰 등록 / 이미지버튼 등록
-    //작가 호칭 - 프로그래스바, 프로그래스바 위 텍스트뷰, 이미지버튼 등록 (+사진도 등록 진척도에 따라 버튼이 활성화 되게끔함)
-    //라디오버튼에 따라 레이아웃의 visible 수정 - 2개니깐 이렇게 하는게 효율적인 듯 ~
-    //그리고 동화 업적에 받은 트로피의 개수도 연동되어야 하니 하나의 자바에서 처리하는게 효율
+    private static final String PREFS_NAME = "AchievementPrefs";
+    private SharedPreferences sharedPreferences;
 
     private RadioGroup rgTrophyCategory;
     private ImageButton ibCloseWindow;
@@ -28,16 +29,11 @@ public class AchieveActivity extends AppCompatActivity implements UserInfoLoader
 
     private UserInfo userInfo = new UserInfo();
 
-    //완료된 동화 테마 개수 세기
-    private int seaTrophyCount, forestTrophyCount,
-                castleTrophyCount, villageTrophyCount,
-                universeTrophyCount, desertTrophyCount,
-                customTrophyCount;
-
-    //동화 테마 목표 개수
-    private int goalSeaTrophyCount, goalForestTrophyCount, goalcastleTrophyCount,
-                goalVillageTrophyCount, goalUniverseTrophyCount, goalDesertTrophyCount,
-                goalCustomTrophyCount;
+    //완료된 동화 테마 개수
+    private int seaTrophyCount = 0, forestTrophyCount = 1,
+                castleTrophyCount = 3, villageTrophyCount = 5,
+                universeTrophyCount = 10, desertTrophyCount = 15,
+                customTrophyCount = 20;
 
     //동화 업적 텍스트뷰
     private TextView tvSeaTrophy, tvSeaTrophyPercent,
@@ -48,35 +44,47 @@ public class AchieveActivity extends AppCompatActivity implements UserInfoLoader
             tvDesertTrophy, tvDesertTrophyPercent,
             tvCustomTrophy, tvCustomTrophyPercent;
 
+    //목표 개수 리스트 설정
+    private int[] trophyGoals = {1, 5, 10, 20, 50};
+    private int seaTrophyIndex=0, forestTrophyIndex=0, castleTrophyIndex=0, villageTrophyIndex=0,
+                universeTrophyIndex=0, desertTrophyIndex=0, customTrophyIndex=0;
+    private ImageButton seaButton, forestButton, villageButton, castleButton, universeButton,
+                        desertButton, customButton;
+
+    //작가호칭 - 프로그래스바 목표
+    private int totalTrophyCount=0;
+    private final int[] totalTrophyGoals = {1, 5, 10, 50};
+    private ProgressBar[] pbTrophy = new ProgressBar[4];
+    private TextView[] tvTrophy = new TextView[4];
+    private ImageButton[] ibTrophy = new ImageButton[4];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achieve);
 
-        //동화업적, 작가호칭 선택 라디오그룹
-        rgTrophyCategory = findViewById(R.id.rg_trophyCategory);
-
-        //이미지버튼
-        ibCloseWindow = findViewById(R.id.ib_closeWindow);
-
-        //동화업적 개수 초기화?
-        seaTrophyCount = 0; forestTrophyCount = 0; castleTrophyCount = 0; villageTrophyCount = 0; universeTrophyCount = 0;
-
         name = findViewById(R.id.mini_name);
         profile = findViewById(R.id.mini_profile);
-
         loadUserInfo(profile, name);
 
-        //동화업적 텍스트뷰 연결
-        tvSeaTrophy = findViewById(R.id.tv_seaTrophy); tvSeaTrophyPercent = findViewById(R.id.tv_seaTrophyPercent);
-        tvForestTrophy = findViewById(R.id.tv_forestTrophy); tvForestTrophyPercent = findViewById(R.id.tv_forestTrophyPercent);
-        tvCastleTrophy = findViewById(R.id.tv_castleTrophy); tvCastleTrophyPercent = findViewById(R.id.tv_castleTrophyPercent);
-        tvVillageTrophy = findViewById(R.id.tv_villageTrophy); tvVillageTrophyPercent = findViewById(R.id.tv_villageTrophyPercent);
-        tvUniverseTrophy = findViewById(R.id.tv_universeTrophy); tvUniverseTrophyPercent = findViewById(R.id.tv_universeTrophyPercent);
+        //UI설정
+        settingUI();
+
+        // SharedPreferences에서 값을 로드
+        loadTrophyCounts();
+        //UI설정 업데이트
+        updateUIForAllThemes();
+        //프로그래스바 업데이트
+        updateProgressBars();
 
         //디폴트 상태 설정 - 처음에 키면 동화업적이 띄워져있는걸로함
         findViewById(R.id.fairytaleAchievementLayout).setVisibility(View.VISIBLE);
         findViewById(R.id.writerappellationLayout).setVisibility(View.GONE);
+
+        // 작가 호칭 보상 버튼을 초기에는 비활성화
+        for (ImageButton btn : ibTrophy) {
+            btn.setEnabled(false);
+        }
 
         //동화업적, 작가호칭 선택 리스너
         rgTrophyCategory.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -102,21 +110,350 @@ public class AchieveActivity extends AppCompatActivity implements UserInfoLoader
             }
         });
 
-        //동화업적창 알고리즘
-        
-        //완료된 바다 숲 궁전 마을 우주 동화 개수 세기
+        seaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭 시 onRewardButtonClicked 호출
+                onRewardButtonClicked("바다");
+            }
+        });
 
-        //만들어질때마다 업데이트 + 그에 맞도록 퍼센트 업데이트 textView.setText("New Text from Java Code");
+        forestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭 시 onRewardButtonClicked 호출
+                onRewardButtonClicked("숲");
+            }
+        });
 
-        //트로피 받기를 하면 창이 갱신되면서 (이때 애니메이션을 넣으면 좋을듯?) 새로 나타나게함
-        //얻은 트로피는 어디에 표시?
+        castleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭 시 onRewardButtonClicked 호출
+                onRewardButtonClicked("궁전");
+            }
+        });
 
-        //작가호칭창
-        //트로피 얻은 개수 저장
-        //트로피 개수만큼 프로그래스바 업데이트
-        //목표치를 채우면 사용버튼의 이미지가 바뀌면서 버튼이 활성화됨 - 이 부분은 버튼의 이미지가 바뀌고 바뀐 이미지면 그에 맞는 행동을 넣어놓음
-        //얻은 호칭은 프로필에 나오도록함(업데이트)
+        villageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭 시 onRewardButtonClicked 호출
+                onRewardButtonClicked("마을");
+            }
+        });
 
+        universeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭 시 onRewardButtonClicked 호출
+                onRewardButtonClicked("우주");
+            }
+        });
+
+        desertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭 시 onRewardButtonClicked 호출
+                onRewardButtonClicked("사막");
+            }
+        });
+
+        customButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭 시 onRewardButtonClicked 호출
+                onRewardButtonClicked("커스텀");
+            }
+        });
+
+        // 버튼 클릭 리스너 설정
+        for (int i = 0; i < totalTrophyGoals.length; i++) {
+            final int index = i;
+            ibTrophy[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    handleTrophyReward(index);
+                }
+            });
+        }
+    }
+
+    // SharedPreferences에서 TrophyCounts 로드
+    private void loadTrophyCounts() {
+        SharedPreferences sharedPreferences = getSharedPreferences("TrophyPrefs", MODE_PRIVATE);
+
+        seaTrophyCount = sharedPreferences.getInt("seaTrophyCount", 0); // 여기서 0은 기본값이지만 기본적으로 설정된 값으로 바꿔줌
+        forestTrophyCount = sharedPreferences.getInt("forestTrophyCount", 1);
+        castleTrophyCount = sharedPreferences.getInt("castleTrophyCount", 3);
+        villageTrophyCount = sharedPreferences.getInt("villageTrophyCount", 5);
+        universeTrophyCount = sharedPreferences.getInt("universeTrophyCount", 10);
+        desertTrophyCount = sharedPreferences.getInt("desertTrophyCount", 15);
+        customTrophyCount = sharedPreferences.getInt("customTrophyCount", 20);
+
+        seaTrophyIndex = sharedPreferences.getInt("seaTrophyIndex", 0);
+        forestTrophyIndex = sharedPreferences.getInt("forestTrophyIndex", 0);
+        castleTrophyIndex = sharedPreferences.getInt("castleTrophyIndex", 0);
+        villageTrophyIndex = sharedPreferences.getInt("villageTrophyIndex", 0);
+        universeTrophyIndex = sharedPreferences.getInt("universeTrophyIndex", 0);
+        desertTrophyIndex = sharedPreferences.getInt("desertTrophyIndex", 0);
+        customTrophyIndex = sharedPreferences.getInt("customTrophyIndex", 0);
+
+        totalTrophyCount = sharedPreferences.getInt("totalTrophyCount", 0);
+    }
+
+    // 업데이트된 TrophyCounts를 SharedPreferences에 저장
+    private void saveTrophyCounts() {
+        SharedPreferences sharedPreferences = getSharedPreferences("TrophyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt("seaTrophyCount", seaTrophyCount);
+        editor.putInt("forestTrophyCount", forestTrophyCount);
+        editor.putInt("castleTrophyCount", castleTrophyCount);
+        editor.putInt("villageTrophyCount", villageTrophyCount);
+        editor.putInt("universeTrophyCount", universeTrophyCount);
+        editor.putInt("desertTrophyCount", desertTrophyCount);
+        editor.putInt("customTrophyCount", customTrophyCount);
+
+        editor.putInt("seaTrophyIndex", seaTrophyIndex);
+        editor.putInt("forestTrophyIndex", forestTrophyIndex);
+        editor.putInt("castleTrophyIndex", castleTrophyIndex);
+        editor.putInt("villageTrophyIndex", villageTrophyIndex);
+        editor.putInt("universeTrophyIndex", universeTrophyIndex);
+        editor.putInt("desertTrophyIndex", desertTrophyIndex);
+        editor.putInt("customTrophyIndex", customTrophyIndex);
+
+        editor.putInt("totalTrophyCount", totalTrophyCount);
+
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadTrophyCounts(); // Load state
+        updateUIForAllThemes(); // Update UI
+        updateProgressBars();
+    }
+
+    private void updateUIForAllThemes() {
+        updateUI("바다");
+        updateUI("숲");
+        updateUI("궁전");
+        updateUI("마을");
+        updateUI("우주");
+        updateUI("사막");
+        updateUI("커스텀");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveTrophyCounts(); // Activity가 일시 정지될 때 값을 저장
+    }
+
+    public void handleTrophyReward(int trophyIndex) {
+        if(ibTrophy[trophyIndex].isEnabled()) { //활성화된 상태에서 눌렀다면
+            //작가호칭 보상
+            Toast.makeText(this, totalTrophyGoals[trophyIndex] + "개 목표 달성! 보상을 받았어요!", Toast.LENGTH_SHORT).show();
+
+            ibTrophy[trophyIndex].setEnabled(false);
+            ibTrophy[trophyIndex].setBackgroundResource(R.drawable.ic_btn_appellation_done); // 완료 이미지로 변경
+
+            // 텍스트뷰 업데이트
+            tvTrophy[trophyIndex].setText("완료");
+
+            // 상태 저장
+            saveTrophyRewardStatus(trophyIndex);
+
+            //UI 업데이트
+            updateProgressBars();
+        }
+    }
+
+    private void saveTrophyRewardStatus(int trophyIndex) {
+        SharedPreferences sharedPreferences = getSharedPreferences("TrophyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        // 저장할 상태
+        editor.putBoolean("trophyReward" + trophyIndex, true);
+
+        editor.apply();
+    }
+
+    private boolean isTrophyRewardClaimed(int trophyIndex) {
+        SharedPreferences sharedPreferences = getSharedPreferences("TrophyPrefs", MODE_PRIVATE);
+        return sharedPreferences.getBoolean("trophyReward" + trophyIndex, false);
+    }
+
+    public void updateProgressBars() {
+        for(int i=0; i < totalTrophyGoals.length; i++) {
+            int goal = totalTrophyGoals[i];
+            int current = Math.min(totalTrophyCount, goal); //둘 중 작은걸 선택 - 이미 목표치를 채우면 100퍼만 되게끔하기 위해 사용
+            int percentage = (int) ((current / (float) goal) * 100); //퍼센트 계산 (프로그래스바)
+
+            //프로그래스바 없데이트
+            pbTrophy[i].setProgress(percentage);
+
+            //텍스트뷰 업데이트
+            String text = String.format("트로피 %d / %d", current, goal);
+            tvTrophy[i].setText(text);
+
+            //목표 달성 시 이미지 버튼 활성화 + 배경 이미지 바꿈
+            if(totalTrophyCount >= goal) {
+                if (!isTrophyRewardClaimed(i)) {
+                    ibTrophy[i].setEnabled(true);
+                    ibTrophy[i].setBackgroundResource(R.drawable.ic_btn_appellation); // 잠금해제 이미지로 변경
+                } else {
+                    ibTrophy[i].setEnabled(false);
+                    ibTrophy[i].setBackgroundResource(R.drawable.ic_btn_appellation_done); // 완료 이미지로 변경
+                }
+            } else {
+                ibTrophy[i].setEnabled(false);
+                ibTrophy[i].setBackgroundResource(R.drawable.ic_btn_appellation_locked); // 잠금 이미지로 변경
+            }
+        }
+    }
+
+    public void updateUI(String thema) {
+        int currentCount = 0;
+        int currentIndex = 0;
+        TextView goalTextView = null;
+        TextView percentageTextView = null;
+
+        if (thema.equals("바다")) {
+            currentCount = seaTrophyCount;
+            currentIndex = seaTrophyIndex;
+            goalTextView = tvSeaTrophy;
+            percentageTextView = tvSeaTrophyPercent;
+        } else if (thema.equals("숲")) {
+            currentCount = forestTrophyCount;
+            currentIndex = forestTrophyIndex;
+            goalTextView = tvForestTrophy;
+            percentageTextView = tvForestTrophyPercent;
+        } else if (thema.equals("궁전")) {
+            currentCount = castleTrophyCount;
+            currentIndex = castleTrophyIndex;
+            goalTextView = tvCastleTrophy;
+            percentageTextView = tvCastleTrophyPercent;
+        } else if (thema.equals("마을")) {
+            currentCount = villageTrophyCount;
+            currentIndex = villageTrophyIndex;
+            goalTextView = tvVillageTrophy;
+            percentageTextView = tvVillageTrophyPercent;
+        } else if (thema.equals("우주")) {
+            currentCount = universeTrophyCount;
+            currentIndex = universeTrophyIndex;
+            goalTextView = tvUniverseTrophy;
+            percentageTextView = tvUniverseTrophyPercent;
+        } else if (thema.equals("사막")) {
+            currentCount = desertTrophyCount;
+            currentIndex = desertTrophyIndex;
+            goalTextView = tvDesertTrophy;
+            percentageTextView = tvDesertTrophyPercent;
+        } else if (thema.equals("커스텀")) {
+            currentCount = customTrophyCount;
+            currentIndex = customTrophyIndex;
+            goalTextView = tvCustomTrophy;
+            percentageTextView = tvCustomTrophyPercent;
+        }
+
+        if (currentCount >= trophyGoals[currentIndex]) {
+            percentageTextView.setText("보상 받기");
+        } else {
+            int percentage = (currentCount * 100) / trophyGoals[currentIndex];
+            percentageTextView.setText(percentage + "%");
+        }
+
+        String goalText = String.format("%s 동화 %d개 만들기", thema, trophyGoals[currentIndex]);
+        goalTextView.setText(goalText);
+    }
+
+    public void onRewardButtonClicked(String thema) {
+        TextView percentageTextView = getPercentageTextView(thema);
+
+        if (percentageTextView != null && percentageTextView.getText().toString().equals("보상 받기")) {
+            totalTrophyCount++;
+            updateTrophyIndex(thema);
+            saveTrophyCounts(); // Save state
+            updateProgressBars();
+        }
+        updateUI(thema);
+    }
+
+    private TextView getPercentageTextView(String thema) {
+        switch (thema) {
+            case "바다": return tvSeaTrophyPercent;
+            case "숲": return tvForestTrophyPercent;
+            case "궁전": return tvCastleTrophyPercent;
+            case "마을": return tvVillageTrophyPercent;
+            case "우주": return tvUniverseTrophyPercent;
+            case "사막": return tvDesertTrophyPercent;
+            case "커스텀": return tvCustomTrophyPercent;
+            default: return null;
+        }
+    }
+
+    private void updateTrophyIndex(String thema) {
+        switch (thema) {
+            case "바다": seaTrophyIndex++; break;
+            case "숲": forestTrophyIndex++; break;
+            case "궁전": castleTrophyIndex++; break;
+            case "마을": villageTrophyIndex++; break;
+            case "우주": universeTrophyIndex++; break;
+            case "사막": desertTrophyIndex++; break;
+            case "커스텀": customTrophyIndex++; break;
+        }
+    }
+
+    public void settingUI() {
+        //동화업적, 작가호칭 선택 라디오그룹
+        rgTrophyCategory = findViewById(R.id.rg_trophyCategory);
+
+        //이미지버튼
+        ibCloseWindow = findViewById(R.id.ib_closeWindow);
+
+        //동화업적 텍스트뷰 연결
+        tvSeaTrophy = findViewById(R.id.tv_seaTrophy);
+        tvSeaTrophyPercent = findViewById(R.id.tv_seaTrophyPercent);
+        tvForestTrophy = findViewById(R.id.tv_forestTrophy);
+        tvForestTrophyPercent = findViewById(R.id.tv_forestTrophyPercent);
+        tvCastleTrophy = findViewById(R.id.tv_castleTrophy);
+        tvCastleTrophyPercent = findViewById(R.id.tv_castleTrophyPercent);
+        tvVillageTrophy = findViewById(R.id.tv_villageTrophy);
+        tvVillageTrophyPercent = findViewById(R.id.tv_villageTrophyPercent);
+        tvUniverseTrophy = findViewById(R.id.tv_universeTrophy);
+        tvUniverseTrophyPercent = findViewById(R.id.tv_universeTrophyPercent);
+        tvDesertTrophy = findViewById(R.id.tv_desertTrophy);
+        tvDesertTrophyPercent = findViewById(R.id.tv_desertTrophyPercent);
+        tvCustomTrophy = findViewById(R.id.tv_customTrophy);
+        tvCustomTrophyPercent = findViewById(R.id.tv_customTrophyPercent);
+
+        //보상버튼
+        seaButton = findViewById(R.id.ib_seaTrophy);
+        forestButton = findViewById(R.id.ib_forestTrophy);
+        castleButton = findViewById(R.id.ib_castleTrophy);
+        villageButton = findViewById(R.id.ib_villageTrophy);
+        universeButton = findViewById(R.id.ib_universeTrophy);
+        desertButton = findViewById(R.id.ib_desertTrophy);
+        customButton = findViewById(R.id.ib_customTrophy);
+
+        //작가호칭 보상 이미지 버튼
+        ibTrophy[0] = findViewById(R.id.ib_littleWriter);
+        ibTrophy[1] = findViewById(R.id.ib_beginnerWriter);
+        ibTrophy[2] = findViewById(R.id.ib_veteranWriter);
+        ibTrophy[3] = findViewById(R.id.ib_masterWriter);
+
+        //작가호칭 프로그래스바
+        pbTrophy[0] = findViewById(R.id.pb_trophyCount1);
+        pbTrophy[1] = findViewById(R.id.pb_trophyCount5);
+        pbTrophy[2] = findViewById(R.id.pb_trophyCount10);
+        pbTrophy[3] = findViewById(R.id.pb_trophyCount50);
+
+        //작가호칭 텍스트뷰
+        tvTrophy[0] = findViewById(R.id.tv_trophyCount1);
+        tvTrophy[1] = findViewById(R.id.tv_trophyCount5);
+        tvTrophy[2] = findViewById(R.id.tv_trophyCount10);
+        tvTrophy[3] = findViewById(R.id.tv_trophyCount50);
     }
 
     @Override
